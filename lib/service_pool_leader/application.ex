f4d7c3b73bd76e18b9b246289765a0aa9287f498
Mark Ericksen <brainlid@gmail.com>
2017-11-15 08:31:26 -0700

    better recovery strategy setup

--- lib/service_pool_leader/application.ex
+++ lib/service_pool_leader/application.ex
@@ -7,7 +7,7 @@ defmodule ServicePoolLeader.Application do
   alias ServicePoolLeader.SimpleService
   alias ServicePoolLeader.Coordinator
   alias ServicePoolLeader.TrackedService
-  alias ServicePoolLeader.RegistryService
+  # alias ServicePoolLeader.RegistryService
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
@@ -17,15 +17,15 @@ defmodule ServicePoolLeader.Application do
       # Starts a worker by calling: ServicePoolLeader.Worker.start_link(arg)
       # {ServicePoolLeader.Worker, arg},
       worker(SimpleService, []),
-      
+
       # Starts a coordinator service that can manage tracking the registered services.
       worker(Coordinator, []),
-      
+
       # Starts the service that uses the Coordinator and registers itself with it.
       worker(TrackedService, []),
 
       # # If you want to try it using a Registry... (does not work in cluster across nodes, local-only)
-      # # Commented out by default as it clutters the :observer.start Application view and doesn't 
+      # # Commented out by default as it clutters the :observer.start Application view and doesn't
       # # work for our goals.
       # supervisor(Registry, [:duplicate, RegistryService.registry_name]),
       # worker(RegistryService, []),
@@ -33,7 +33,11 @@ defmodule ServicePoolLeader.Application do
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: ServicePoolLeader.Supervisor]
+    # opts = [strategy: :one_for_one, name: ServicePoolLeader.Supervisor]
+
+    # Using :rest_for_one, if a Coordinator goes down, restart the tracked
+    # service. When it starts, it re-registers with the Coordinator.
+    opts = [strategy: :rest_for_one, name: ServicePoolLeader.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
